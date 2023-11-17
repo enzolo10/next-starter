7d0ce128d71660bd29a4e57d1058e04cbe3dc652
enzo.lo <enzo.lo@mlytics.com>
2023-11-17 18:09:41 +0800

    build: Update i18n

---
+++ src/app/layout.tsx
@@ -0,0 +1,8 @@
+import { PropsWithChildren } from 'react';
+import '@/app/globals.css';
+
+const RootLayout = ({ children }: PropsWithChildren) => {
+  return children;
+};
+
+export default RootLayout;
--- src/i18n.ts
+++ src/i18n.ts
@@ -1,6 +1,6 @@
 import { getRequestConfig } from 'next-intl/server';
 
-export const locales = ['en', 'zh-hant'] as const;
+export const locales = ['en', 'zh-hant'];
 
 export default getRequestConfig(async ({ locale }) => ({
   messages: (await import(`@/i18n/${locale}.json`)).default,
